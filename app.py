3b9c31a05d5ed77aab8b84fe0288f86ad36ad1ec
AW_Dev <riptidegamesceo@gmail.com>
2024-07-31 12:48:53 -0700

    add code to hopefully remove cache

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 import requests
+from functools import wraps, update_wrapper
+from datetime import datetime
 
 app = Flask(__name__,
             static_url_path='/resources',
@@ -11,11 +13,26 @@ access_token = '1a0f8b8243b980d0407f15535f6518b2'
 graphql_endpoint = f'https://{store_name}.myshopify.com/api/2024-07/graphql.json'
 
 
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return update_wrapper(no_cache, view)
+
+
+@nocache
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
+@nocache
 @app.route('/product/<id>')
 def product(id):
     query = """
